94ff3b44556817b24f2d80b762fc3034e4a1a8f9
Nate Marti <nate_marti@sil.org>
2024-02-10 07:18:00 +0100

    remove debug option from build script

--- scripts/build-pyinstaller.sh
+++ scripts/build-pyinstaller.sh
@@ -3,11 +3,6 @@
 scripts_dir="$(dirname "$0")"
 source "${scripts_dir}/build--set-env.sh"
 
-DEBUG=
-if [[ $1 == '-d' || $1 == '--debug' ]]; then
-    DEBUG=--debug=bootloader
-fi
-
 # Ensure starting in repo root.
 if [[ $PWD != "$REPO_DIR" ]]; then
     echo "Error: $0 must be run from the root of the repo."
@@ -28,4 +23,4 @@ fi
 
 # Install app locally to create executable script.
 pip install .
-pyinstaller --clean $DEBUG chorushub.spec
+pyinstaller --clean chorushub.spec
